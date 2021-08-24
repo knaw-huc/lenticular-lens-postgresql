9f5cb60203ff2e0b9ecf097d7065b164021951d7
kerim1 <kerim1@users.noreply.github.com>
2021-08-24 09:11:30 +0200

    Rename t-conorm to s-norm

--- sql/logic_ops.sql
+++ sql/logic_ops.sql
@@ -22,27 +22,27 @@ CREATE FUNCTION t_h0(a numeric, b numeric) RETURNS numeric AS $$
 SELECT CASE WHEN NOT a = 0 AND NOT b = 0 THEN a * b / (a + b - (a * b)) ELSE 0 END;
 $$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE FUNCTION tc_max(a numeric, b numeric) RETURNS numeric AS $$
+CREATE FUNCTION s_max(a numeric, b numeric) RETURNS numeric AS $$
 SELECT greatest(a, b);
 $$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE FUNCTION tc_sum(a numeric, b numeric) RETURNS numeric AS $$
+CREATE FUNCTION s_sum(a numeric, b numeric) RETURNS numeric AS $$
 SELECT a + b - (a * b);
 $$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE FUNCTION tc_luk(a numeric, b numeric) RETURNS numeric AS $$
+CREATE FUNCTION s_luk(a numeric, b numeric) RETURNS numeric AS $$
 SELECT least(a + b, 1);
 $$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE FUNCTION tc_d(a numeric, b numeric) RETURNS numeric AS $$
+CREATE FUNCTION s_d(a numeric, b numeric) RETURNS numeric AS $$
 SELECT CASE WHEN b = 0 THEN a WHEN a = 0 THEN b ELSE 1 END;
 $$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE FUNCTION tc_nm(a numeric, b numeric) RETURNS numeric AS $$
+CREATE FUNCTION s_nm(a numeric, b numeric) RETURNS numeric AS $$
 SELECT CASE WHEN a + b < 1 THEN greatest(a, b) ELSE 1 END;
 $$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
 
-CREATE FUNCTION tc_h2(a numeric, b numeric) RETURNS numeric AS $$
+CREATE FUNCTION s_h2(a numeric, b numeric) RETURNS numeric AS $$
 SELECT (a + b) / (1 + a * b);
 $$ LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE;
 
@@ -52,10 +52,10 @@ CREATE AGGREGATE t_luk_agg(numeric) (sfunc = t_luk, stype = numeric);
 CREATE AGGREGATE t_d_agg(numeric) (sfunc = t_d, stype = numeric);
 CREATE AGGREGATE t_nm_agg(numeric) (sfunc = t_nm, stype = numeric);
 CREATE AGGREGATE t_h0_agg(numeric) (sfunc = t_h0, stype = numeric);
-CREATE AGGREGATE tc_max_agg(numeric) (sfunc = tc_max, stype = numeric);
-CREATE AGGREGATE tc_sum_agg(numeric) (sfunc = tc_sum, stype = numeric);
-CREATE AGGREGATE tc_luk_agg(numeric) (sfunc = tc_luk, stype = numeric);
-CREATE AGGREGATE tc_d_agg(numeric) (sfunc = tc_d, stype = numeric);
-CREATE AGGREGATE tc_nm_agg(numeric) (sfunc = tc_nm, stype = numeric);
-CREATE AGGREGATE tc_h2_agg(numeric) (sfunc = tc_h2, stype = numeric);
+CREATE AGGREGATE s_max_agg(numeric) (sfunc = s_max, stype = numeric);
+CREATE AGGREGATE s_sum_agg(numeric) (sfunc = s_sum, stype = numeric);
+CREATE AGGREGATE s_luk_agg(numeric) (sfunc = s_luk, stype = numeric);
+CREATE AGGREGATE s_d_agg(numeric) (sfunc = s_d, stype = numeric);
+CREATE AGGREGATE s_nm_agg(numeric) (sfunc = s_nm, stype = numeric);
+CREATE AGGREGATE s_h2_agg(numeric) (sfunc = s_h2, stype = numeric);
 
